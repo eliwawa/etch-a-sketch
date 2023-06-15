91e454fe98f26b67770aecf7b1871e0a3cfa0f98
Eli Wawersik <ewawersik@gmail.com>
2023-06-15 10:07:30 -0400

    Add button to create new grid and ask for specified num of pixel

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 const gridContainer = document.querySelector('#grid-container');
+const newGrid = document.querySelector('#new-grid');
 
 function generateGrid(numPixels) {
-    for(i = 0; i < numPixels; i++) {
-        for(j = 0; j < numPixels; j++) {
-            const pixel = document.createElement('div');
-            gridContainer.appendChild(pixel);
-            pixel.setAttribute('col', `${j}`);
-            pixel.setAttribute('row', `${i}`)
-            pixel.classList.add('pixel');
-            pixel.addEventListener("mouseover", (event) => {
-                event.target.style.backgroundColor = 'black';
-            })
-            pixel.style.width = `${(100/numPixels)}%`;
+    if (numPixels > 100) {
+        generateGrid(prompt('Please select a number less than 100'));
+    } else {
+        for(i = 0; i < numPixels; i++) {
+            for(j = 0; j < numPixels; j++) {
+                const pixel = document.createElement('div');
+                gridContainer.appendChild(pixel);
+                pixel.setAttribute('col', `${j}`);
+                pixel.setAttribute('row', `${i}`)
+                pixel.classList.add('pixel');
+                pixel.addEventListener("mouseover", (event) => {
+                    event.target.style.backgroundColor = 'black';
+                })
+                pixel.style.width = `${(100/numPixels)}%`;
+            }
         }
     }
 }
 
-generateGrid(16);
+newGrid.addEventListener('click', (event) => {
+    generateGrid(prompt('How many pixels?'));
+})
